e5cbd8dc19b8ce9ce339ab73ea35d18e7a1b8ff0
Angie Vanegas P <arco527@gmail.com>
2024-06-24 19:46:04 +0000

    Result python exercises

--- exercises/02-What-is-a-function/app.py
+++ exercises/02-What-is-a-function/app.py
@@ -1,6 +1,7 @@
 def sum(number1,number2):
-    return number1 + number2
+    super_duper = number1 + number2
+    return super_duper
 
 # Your code here
-total = sum(2,3)
+total = sum(3445324,53454423)
 print(total)
